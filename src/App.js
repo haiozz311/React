a9229f9fb284a1f1fc43d3171e3036bf6cdedac2
Hai <haiozz311@gmail.com>
2020-07-27 16:43:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './Container/Header/index'
+
 function App() {
   return (
     <div className="App">
-      <Header />
+      
     </div>
   );
 }
