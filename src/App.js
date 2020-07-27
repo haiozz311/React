076c0ffb0e2b52e2e4c115049a127734b6e3b6ff
hoang2692 <hoang2692@gmail.com>
2020-07-27 16:30:11 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-
+import Header from './Container/Header/index'
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
